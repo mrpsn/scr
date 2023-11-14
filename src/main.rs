54ca0edb575d5c51412c00522ff6dd8428943efd
Phil <phil_strange@yahoo.co.uk>
2023-11-14 23:32:13 +0000

    speed related tweeks

--- src/main.rs
+++ src/main.rs
@@ -77,16 +77,19 @@ async fn scan_dir(
     if let Ok(dir_iter) = std::fs::read_dir(path) {
         for r in dir_iter {
             match r {
+
                 Ok(e) if e.file_type().is_ok_and(|f| f.is_dir()) => tx_dir.send(
                     Dir{path: e.path(), tx_dir: tx_dir.clone(), tx_file: tx_file.clone()})
                                 .expect("failed to send dir on channel"),
-                Ok(e) if e.metadata().is_ok_and(|m| m.len() >= min_size) => {
+
+                Ok(e) if e.metadata().is_ok_and(|m| m.len() >= min_size) =>
                     tx_file.send(
                         Filesize::from(e.path())).map_or_else(
-                        |_| errors +=1, |_| files +=1);
-                },
+                        |_| errors +=1, |_| files +=1),
+
                 Ok(e) if e.metadata().is_err() => errors += 1,
-                _ => files +=1,
+
+                _ => files +=1,  // file loaded ok, but < the minimum size
             }
         };
     };
@@ -113,6 +116,13 @@ fn print_files(n: usize, min_size: Arc<AtomicU64>, mut rx_file: UnboundedReceive
                 while entries.len() > n {
                     entries.pop();
                 }
+
+                if entries.len() == n {
+                    if let Some(entry) = entries.last() {
+                        min_size.store(entry.0.size, SeqCst);
+                    }
+                }
+
                 let n_lines = n.min(entries.len());
                 for (i, entry) in entries[idx..n_lines].iter().enumerate() {
                     let formatted_size = entry.0.size.to_formatted_string(&Locale::en);
@@ -121,12 +131,6 @@ fn print_files(n: usize, min_size: Arc<AtomicU64>, mut rx_file: UnboundedReceive
                         entry.0.created, entry.0.modified, entry.0.used, entry.0.path
                     );
                     printer.print_line(line, (idx + i) as u16);
-                }
-
-                if entries.len() == n {
-                    if let Some(entry) = entries.last() {
-                        min_size.store(entry.0.size, SeqCst);
-                    }
                 }
             }
         }
